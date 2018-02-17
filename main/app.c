47fa9dafb51c9ee1a9fcb5d80ae3b7c96f5fd2a2
Patrick Hobusch <phobusch@elegosoft.com>
2018-02-17 23:51:20 +0100

    Basic bedlight

--- main/app.c
+++ main/app.c
@@ -3,189 +3,46 @@
 static const char *TAG = "APP";
 
 static WS2812_stripe_t stripe;
-static const uint8_t stripe_length = 255;
+static const uint8_t stripe_length = 20;
 
-void WIFI_connected()
-{
-    printf("Wifi connected\n");
-}
+static i2c_port_t i2c_port0 = I2C_NUM_0;
 
-void WIFI_disconnected()
-{
-    printf("Wifi disconnected\n");
-}
-
-void messageArrived(MessageData* data)
-{
-    printf("Message arrived on topic %.*s: %.*s\n",
-            data->topicName->lenstring.len,
-            (char *) data->topicName->lenstring.data,
-            data->message->payloadlen,
-            (char *) data->message->payload);
-}
-
-void MQTT_task(void *pvParameters)
-{
-    MQTTClient client;
-    Network network;
-    unsigned char sendbuf[80], readbuf[80];
-    int rc = 0;
-    int count = 0;
-
-    MQTTPacket_connectData connectData = MQTTPacket_connectData_initializer;
-    NetworkInit(&network);
-    MQTTClientInit(&client, &network, 30000, sendbuf, sizeof(sendbuf), readbuf, sizeof(readbuf));
-
-    if ((rc = NetworkConnect(&network, MQTT_HOST, MQTT_PORT)) != 0)
-        printf("Return code from network connect is %d\n", rc);
-
-    /*
-    if (MQTT_TASK) {
-        if ((rc = MQTTStartTask(&client)) != pdPASS)
-            printf("Return code from start tasks is %d\n", rc);
-    }
-    */
-
-    connectData.MQTTVersion = 3;
-    connectData.clientID.cstring = MQTT_CLIENT_ID;
-    connectData.username.cstring = MQTT_USERNAME;
-    connectData.password.cstring = MQTT_PASSWORD;
-
-    if ((rc = MQTTConnect(&client, &connectData)) != 0)
-        printf("Return code from MQTT connect is %d\n", rc);
-    else
-        printf("MQTT Connected\n");
-
-    if ((rc = MQTTSubscribe(&client, "FreeRTOS/sample/#", 2, messageArrived)) != 0)
-        printf("Return code from MQTT subscribe is %d\n", rc);
-
-    while (++count)
-    {
-        MQTTMessage message;
-        char payload[30];
-
-        message.qos = 1;
-        message.retained = 0;
-        message.payload = payload;
-        sprintf(payload, "message number %d", count);
-        message.payloadlen = strlen(payload);
-
-        if ((rc = MQTTPublish(&client, "FreeRTOS/sample/a", &message)) != 0)
-            printf("Return code from MQTT publish is %d\n", rc);
-
-        if (MQTT_TASK) {
-            if ((rc = MQTTYield(&client, 10000)) != 0)
-                printf("Return code from yield is %d\n", rc);
-        }
-
-        TickType_t xDelay = 500 / portTICK_PERIOD_MS;
-        vTaskDelay(xDelay);
-    }
-}
-
-void WS2812_task(void *pvParameters) {
-    uint8_t add = 0;
-    uint8_t sub = 2;
-
-    uint8_t step = 4;
-
-    while (1) {
-        WS2812_color_t last;
-
-        for (uint16_t i = stripe_length; i != 0; i--) {
-            WS2812_get_color(&stripe, i-1, &last);
-            WS2812_set_color(&stripe, i, &last);
-        }
-
-        uint8_t new[3] = { last.r, last.g, last.b };
-
-        // if uint8_t running over
-        if ((new[add] + step) % 255 < new[add]) {
-            ESP_LOGI(TAG, "INC SUB UND ADD");
-            sub = add;
-            add = (add + 1) % 3;
-        }
-
-        new[add] = new[add] + step;
-
-        // if uint8_t not running over
-        if (new[sub] >= step) {
-            new[sub] = new[sub] - step;
-        }
-
-        // ESP_LOGI(TAG, "New color: %d %d %d", new[0], new[1], new[2]);
-
-        WS2812_color_t color = { new[0], new[1], new[2] };
-        WS2812_set_color(&stripe, 0, &color);
-
-        WS2812_write(&stripe);
-        // vTaskDelay(50 / portTICK_PERIOD_MS);
-    }
-}
-
-void I2C_task(void *pvParameters)
-{
-
-}
+static WS2812_color_t warmwhite = { 255, 150, 70 };
 
 void app_main()
 {
     esp_err_t esp_err;
+    
+    
+    WIFI_init(WIFI_MODE_STA, NULL);
+    gpio_install_isr_service(0);
 
-    nvs_flash_init();
+    // Init WS2812 stripe
 
-    // WIFI_init(NULL);
-
-    /*
-     * Init WS2812 stripe
-     */
-
-    /*
-    stripe.gpio_num = GPIO_NUM_22;
+    stripe.gpio_num = WS2812_GPIO;
     stripe.length = stripe_length;
     stripe.rmt_channel = RMT_CHANNEL_0;
     stripe.rmt_interrupt_num = 0;
 
     esp_err = WS2812_init(&stripe);
     if (!esp_err) {
-        ESP_LOGI(TAG, "WS2812 init done");
-        xTaskCreate(&WS2812_task, "WS2812_task", 2048, NULL, 10, NULL);
+        for (uint8_t x = 0; x <  stripe.length; x++) {
+            WS2812_set_color(&stripe, x, &warmwhite);
+        }
+        WS2812_write(&stripe);
     }
-    */
 
-    /*
-     * Init I2C bus and sensors
-     */
+    // Init I2C bus and sensors
 
-    i2c_port_t i2c_port0 = I2C_NUM_0;
-
+    return;
+    
     i2c_config_t i2c_port0_conf;
     i2c_port0_conf.mode = I2C_MODE_MASTER;
-    i2c_port0_conf.sda_io_num = I2C_P0_SDA_GPIO_NUM;
+    i2c_port0_conf.sda_io_num = I2C_P0_GPIO_SDA;
     i2c_port0_conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-    i2c_port0_conf.scl_io_num = I2C_P0_SCL_GPIO_NUM;
+    i2c_port0_conf.scl_io_num = I2C_P0_GPIO_SCL;
     i2c_port0_conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-    i2c_port0_conf.master.clk_speed = 100000;
+    i2c_port0_conf.master.clk_speed = 1600000;
 
     I2C_init(i2c_port0, &i2c_port0_conf);
-    BME280_init(i2c_port0, BME280_ADDR_HIGH);
-
-    // TODO: How to decide if update should be started?
-    // xTaskCreate(&OTA_task, "OTA_task", 2048, NULL, 10, NULL);
-
-    /*
-    esp_bt_controller_init();
-
-    if (esp_bt_controller_enable(ESP_BT_MODE_BTDM) != ESP_OK) {
-        return;
-    }
-
-    ESP_LOGI("TAG", "TEST");
-
-    xTaskCreate(&bleAdvtTask, "bleAdvtTask", 2048, (void* ) 0, 10, NULL);
-//    xTaskCreate(&wifiTask,    "wifiTask",    2048, (void* ) 1, 10, NULL);
-     */
-
-    // don't call vTaskStartScheduler() because
-    // the scheduler is already running per default
 }
--- main/app.h
+++ main/app.h
@@ -1,17 +1,40 @@
 #ifndef MAIN_APP_H
 #define MAIN_APP_H
 
+#include "esp_attr.h"
+#include "esp_err.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
 #include "esp_log.h"
+#include "esp_system.h"
+#include "esp_task_wdt.h"
+#include "esp_types.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
 
 #include "driver/gpio.h"
 #include "driver/rmt.h"
 
-#include "wifi.h"
-#include "ota.h"
-#include "ws2812.h"
-
 #include "i2c.h"
+#include "delay.h"
+#include "ota.h"
+#include "wifi.h"
+#include "ws2812.h"
+#include "display/ssd1306_i2c.h"
+#include "display/keyboard_64x128.h"
+#include "font/lcd5x7.h"
 #include "sensor/bme280_i2c.h"
+#include "sensor/rotary_encoder.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/portmacro.h"
+#include "freertos/projdefs.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+
+#include <time.h>
+#include <string.h>
+#include <sys/time.h>
 
 #include "MQTTESP32.h"
 #include "MQTTClient.h"
@@ -20,10 +43,12 @@
  * Keep GPIO defines ordered by GPIO_NUM_XX to avoid double assignments
  */
 
-#define I2C_P0_SCL_GPIO_NUM    GPIO_NUM_16    // pin 25
-#define I2C_P0_SDA_GPIO_NUM    GPIO_NUM_5     // pin 34
-
-#define WS2812_GPIO_NUM        GPIO_NUM_22    // pin 39
+const gpio_num_t ROTENC_GPIO_CLK   = GPIO_NUM_16;   // pin
+const gpio_num_t ROTENC_GPIO_DT    = GPIO_NUM_17;   // pin
+const gpio_num_t ROTENC_GPIO_SW    = GPIO_NUM_18;   // pin
+const gpio_num_t I2C_P0_GPIO_SDA   = GPIO_NUM_26;
+const gpio_num_t I2C_P0_GPIO_SCL   = GPIO_NUM_27;
+const gpio_num_t WS2812_GPIO       = GPIO_NUM_33;
 
 /*
  * Other defines
@@ -34,6 +59,6 @@
 #define MQTT_CLIENT_ID CONFIG_MQTT_CLIENT_ID
 #define MQTT_USERNAME  CONFIG_MQTT_USERNAME
 #define MQTT_PASSWORD  CONFIG_MQTT_PASSWORD
-#define MQTT_TASK      CONFIG_MQTT_TASK
+#define MQTT_USE_TASK  CONFIG_MQTT_USE_TASK
 
 #endif
