ab413fc9e0885e5d4758d3fd020dc0bf61192341
Patrick Hobusch <phobusch@elegosoft.com>
2018-02-18 21:22:26 +0100

    Allow setting stripe via button or websocket simutaneously

--- main/app.c
+++ main/app.c
@@ -3,11 +3,15 @@
 static const char *TAG = "APP";
 
 static WS2812_stripe_t stripe;
+static volatile uint8_t stripe_state = 0; 
 static const uint8_t stripe_length = 56;
 
 static i2c_port_t i2c_port0 = I2C_NUM_0;
 
 static WS2812_color_t warmwhite = { 255, 150, 70 };
+
+static QueueHandle_t gpio_intr_queue = NULL;
+static unsigned long gpio_intr_last = 0;
 
 static void on()
 {
@@ -30,6 +34,9 @@ static void on()
 
         delay_us(delay >> i);
     }
+
+    stripe_state = 1;
+    ESP_LOGI(TAG, "LED on");
 }
 
 static void off()
@@ -53,6 +60,38 @@ static void off()
 
         delay_us(delay >> (i-offset));
     }
+
+    stripe_state = 0;
+    ESP_LOGI(TAG, "LED off");
+}
+
+static void toggle()
+{
+    stripe_state == 0 ? on() : off();
+}
+
+static void IRAM_ATTR gpio_isr(void* args)
+{
+    gpio_num_t gpio_num = (gpio_num_t) args;
+
+    unsigned long us = micros();
+
+    if (us - gpio_intr_last > 100000) {
+        xQueueSendFromISR(gpio_intr_queue, &gpio_num, NULL);
+        gpio_intr_last = us;
+    }
+}
+
+static void IRAM_ATTR gpio_isr_task(void* pvParams)
+{
+    gpio_num_t gpio_num;
+    BaseType_t x = pdFALSE;
+    unsigned long us = 0;
+
+    while(1) {
+        xQueueReceive(gpio_intr_queue, &gpio_num, portMAX_DELAY);
+        toggle();
+    }
 }
 
 static void ledWebsocketRecv(
@@ -63,10 +102,10 @@ static void ledWebsocketRecv(
 {
     if (len == 5 && strncmp(data, "false", 5) == 0) {
         off();
-        ESP_LOGI(TAG, "LED off");
     } else if (len == 4 && strncmp(data, "true", 4) == 0) {
         on();
-        ESP_LOGI(TAG, "LED on");
+    } else {
+        toggle();
     }
 }
 
@@ -92,10 +131,11 @@ void app_main()
     esp_err_t esp_err;
     
     WIFI_init(WIFI_MODE_AP, NULL);
+    gpio_install_isr_service(0);
 
 	espFsInit((void*)(webpages_espfs_start));
 
-    uint8_t maxConnections = 8;
+    uint8_t maxConnections = 16;
     httpd_buffer = malloc(sizeof(RtosConnType) * maxConnections);
 
     httpdFreertosInit(
@@ -117,6 +157,18 @@ void app_main()
     if (!esp_err) {
         on();
     }
+
+    gpio_config_t gpio_conf;
+    gpio_conf.intr_type = GPIO_PIN_INTR_POSEDGE;
+    gpio_conf.pin_bit_mask = BIT(0);
+    gpio_conf.mode = GPIO_MODE_INPUT;
+    gpio_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+    gpio_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    gpio_config(&gpio_conf);
+
+    gpio_intr_queue = xQueueCreate(10, sizeof(gpio_num_t));
+    xTaskCreate(gpio_isr_task, "gpio_isr_task", 4096, NULL, (1 | portPRIVILEGE_BIT), NULL);
+    gpio_isr_handler_add(0, gpio_isr, (void *) 0);
 
     return;
 
